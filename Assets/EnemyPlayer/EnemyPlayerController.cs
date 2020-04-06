9e333a6f7051da73499892f99ac86ce8125754b2
m7waaaath <m7dshadow2@gmail.com>
2020-04-06 23:41:25 +0300

    بارتكل ايفكت مبدئي

--- Assets/EnemyPlayer/EnemyPlayerController.cs
+++ Assets/EnemyPlayer/EnemyPlayerController.cs
@@ -7,18 +7,24 @@ public class EnemyPlayerController : MonoBehaviour
     private Rigidbody2D rb;
     public float speed;
     private Vector2 moveAmount;
-
-
+    //sneeze
+    public GameObject Sneeze;
+    //
     void Start()
     {
-
         rb = GetComponent<Rigidbody2D>();
-
     }
 
 
     void Update()
     {
+        //sneeze
+         
+        if (Input.GetKey(KeyCode.C))
+        {
+            Invoke("destroyProjectile", 0.1f);
+        }
+        //
         Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         moveAmount = moveInput.normalized * speed;
     }
@@ -27,4 +33,13 @@ public class EnemyPlayerController : MonoBehaviour
         rb.MovePosition(rb.position + moveAmount * Time.fixedDeltaTime);
     }
 
+
+     void destroyProjectile()
+    {
+        
+        Instantiate(Sneeze, transform.position, Quaternion.identity);
+        Destroy(Sneeze);
+        
+    }
 }
+
