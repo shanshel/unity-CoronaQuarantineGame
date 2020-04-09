d28536d4e636815a2049ff5fe6e3c6700c25da22
m7waaaath <m7dshadow2@gmail.com>
2020-04-09 20:27:39 +0300

    الشخصية مع الانميشن

--- Assets/EnemyPlayer/EnemyPlayerController.cs
+++ Assets/EnemyPlayer/EnemyPlayerController.cs
@@ -7,13 +7,15 @@ public class EnemyPlayerController : MonoBehaviour
     private Rigidbody2D rb;
     public float speed;
     private Vector2 moveAmount;
-    //sneeze
-    public GameObject Sneeze;
+    public GameObject weopon;
+    private bool isSneezing = true;
     private float timer;
-    //
+    private float movingTIMER = 0;
+    //animation
+    private Animator anim;
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
     }
 
@@ -22,32 +24,75 @@ public class EnemyPlayerController : MonoBehaviour
     {
         timer += Time.deltaTime;
         //sneeze
-        if (Input.GetKeyDown(KeyCode.C))
-        {
-            timer = 0;
-            
-            Invoke("destroyProjectile", 0.1f);
-        }
-        if(timer >= 10)
-        {
-            Invoke("destroyProjectile", 0.1f);
-            timer = 0;
-        }
-        //
-        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        moveAmount = moveInput.normalized * speed;
+        sneezeFN();
+        //animation + movement
+        AnimationFN();
+        //weopne rotation
+        weoponFN();
     }
     private void FixedUpdate()
     {
         rb.MovePosition(rb.position + moveAmount * Time.fixedDeltaTime);
     }
+    public void sneezeFN()
+        {
+        
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            anim.SetTrigger("Sneez");
+            timer = 0;
+            isSneezing = false;
 
 
-     void destroyProjectile()
+          //  Invoke("destroyProjectile", 0.1f);
+        }
+        if (timer >= 10)
+        {
+          //  Invoke("destroyProjectile", 0.1f);
+            timer = 0;
+        }
+    }
+
+    public void AnimationFN()
+    {
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveAmount = moveInput.normalized * speed;
+        if (moveInput != Vector2.zero)
+        {
+            anim.SetBool("isRunning", true);
+        }
+        else
+        {
+            anim.SetBool("isRunning", false);
+        }
+        if(isSneezing == false)
+        {
+            moveAmount = Vector2.zero;
+            
+            movingTIMER += Time.deltaTime;
+            if(movingTIMER >= 0.1)
+            {
+                isSneezing = true;
+                movingTIMER = 0;
+                
+            }
+        }
+    }
+
+    void weoponFN()
+    {
+        Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - weopon.transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        weopon.transform.rotation = rotation;
+    }
+
+
+    void destroyProjectile()
     {
         
-        Instantiate(Sneeze, transform.position, Quaternion.identity);
-        Destroy(Sneeze);
+       // Instantiate(Sneeze, transform.position, Quaternion.identity);
+       // Destroy(Sneeze);
         
     }
 }
