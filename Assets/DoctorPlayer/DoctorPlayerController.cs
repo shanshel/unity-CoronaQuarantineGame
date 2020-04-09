bce904e47c877a9a81bcd705359ca706e58902ce
shanshel6 <shanshel6@gmail.com>
2020-04-09 11:38:47 +0300

    كنترولر الدكتور, تحرك بالاتجاهات

--- Assets/DoctorPlayer/DoctorPlayerController.cs
+++ Assets/DoctorPlayer/DoctorPlayerController.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class DoctorPlayerController : MonoBehaviour
 {
+    public float speed;
+    private Rigidbody2D rb;
+    private Vector2 moveAmount;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveAmount = moveInput.normalized * speed;
+    }
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveAmount * Time.fixedDeltaTime);
     }
 }
