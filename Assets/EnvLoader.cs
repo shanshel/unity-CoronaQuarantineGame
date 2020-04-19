88a8596173aaf0ac84b99bcd996a00fd8505c001
Yousif Shanshel <yousif.shanshel360@gmail.com>
2020-04-19 15:14:14 +0300

    - place more env props

---
+++ Assets/EnvLoader.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision.name);
+        if (collision.tag == "Props")
+        {
+           // collision.gameObject.GetComponent<SpriteRenderer>().enabled = true;
+        }
+        
+       
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Props")
+        {
+            //collision.gameObject.GetComponent<SpriteRenderer>().enabled = false;
+        }
+    }
+}
