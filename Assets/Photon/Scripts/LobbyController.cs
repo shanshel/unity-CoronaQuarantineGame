7cb6e465df7e5f5de512213d8619e2d8f5fae5db
Yousif Shanshel <yousif.shanshel360@gmail.com>
2020-04-22 13:02:04 +0300

    multiplayer1

---
+++ Assets/Photon/Scripts/LobbyController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Realtime;
+using Photon.Pun;
+public class LobbyController : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    public bool canJoinRoom;
+
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+
+
+ 
+
+
+ 
+}
---
+++ Assets/Photon/Scripts/RoomController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class RoomController : MonoBehaviourPunCallbacks
+{
+    public int maxPlayerCount;
+    public List<RoomInfo> roomList = new List<RoomInfo>();
+    public string currentRoomName;
+    string lastRoomName;
+    bool lastIsPublic;
+
+
+    public void createRoom(string roomName, bool isPublic)
+    {
+        RoomOptions roomOption = new RoomOptions();
+        roomOption.MaxPlayers = (byte)maxPlayerCount;
+        roomOption.IsVisible = isPublic;
+        roomOption.IsOpen = true;
+
+        lastRoomName = roomName;
+        lastIsPublic = isPublic;
+        currentRoomName = roomName + getRandomRoomNumber();
+        PhotonNetwork.CreateRoom(currentRoomName, roomOption);
+    }
+
+    public string getRandomRoomNumber()
+    {
+        return Random.Range(0, 10000).ToString();
+    }
+
+    /* Callbacks */
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        this.roomList = roomList;
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("RoomCreatedFailed");
+        createRoom(lastRoomName, lastIsPublic);
+    }
+
+ 
+
+}
