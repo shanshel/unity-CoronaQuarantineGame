fda3833f0920732ab7828dfd9377e07f725378ee
Yousif Shanshel <yousif.shanshel360@gmail.com>
2020-04-24 13:23:33 +0300

    UI basics

---
+++ Assets/Managers/EnumsData.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnumsData
+{
+    public enum SFXEnum
+    {
+        dash, rotate, scaleUp, scaleDown, pass, hited, spawnObst,
+        monsterLaugh, lose, gain, clap, firework, wallTouchedTop,
+    }
+
+    public enum WindowEnum
+    {
+       Main, CustomizeOption, DoctorCustomizition, PatientCustomizition, ServerList, InRoom
+
+    }
+
+    public enum PopupEnum
+    {
+        ServerCreate, ServerInfo, Info, Error
+    }
+
+}
+
--- Assets/Managers/SharedGameManager.cs
+++ Assets/Managers/SharedGameManager.cs
@@ -1,9 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using DG.Tweening;
 public class SharedGameManager : MonoBehaviour
 {
+    private static SharedGameManager _instance;
+    public static SharedGameManager _inst { get { return _instance; } }
+
+
+    private void Awake()
+    {
+
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+            DOTween.defaultAutoPlay = AutoPlay.None;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
